8faf472317732570e97c40bf197a0a93cd3b9863
1C-MY <1573200474@qq.com>
2026-01-03 18:12:38 +0800

    修改

--- src/config.ts
+++ src/config.ts
@@ -22,8 +22,8 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "中文测试",
-	subtitle: "这是一个副标题",
+	title: "画心",
+	subtitle: "CS2",
 	siteURL: "https://xn--15tu19b.shop/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-01-03", // 站点开始运行日期，用于站点统计组件计算运行天数
 
@@ -138,7 +138,7 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "美しいミズキ", // 主页横幅主标题
+			title: "主标题", // 主页横幅主标题
 
 			subtitle: [
 				"特別なことはないけど、君がいると十分です",
