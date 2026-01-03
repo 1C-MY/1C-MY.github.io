efdb4db69565817e0f6d83bd2848f833429f9873
1C-MY <1573200474@qq.com>
2026-01-03 18:18:54 +0800

    修改

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "画心",
-	subtitle: "CS2",
+	subtitle: "CS",
 	siteURL: "https://xn--15tu19b.shop/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-01-03", // 站点开始运行日期，用于站点统计组件计算运行天数
 
